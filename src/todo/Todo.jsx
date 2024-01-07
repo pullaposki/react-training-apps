5eb33744239baeed462586f83744172ce7f8a92f
pullaposki <pullaposki@windowslive.com>
2024-01-07 15:06:33 +0200

    Refactor Todo component and add styles
    Updated the Todo component to initialize state with id-added objects and refactored the task adding process. CSS module has been introduced for better UI presentation. The task checking system has been also revamped to use unique identifiers (UUID) instead of task names.

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,72 +1,71 @@
-﻿import {useState} from "react";
-import data from "./data.json"
+﻿import { useState } from "react";
+import data from "./data.json";
 import { v4 as uuidv4 } from 'uuid'; // for generating ids
+import styles from "./Todo.module.css";  // import styles
 
-function Todo(){
-    const [tasks, setTasks] = useState(data);
-    const [newTaskName, setNewTaskName] = useState("new task");
-
-
-    generateIdsForTasks();
-    function generateIdsForTasks(){
-        let newTasks = tasks.map(function (task){
-            let newTask = {...task}
+function Todo() {
+    const generateDataWithIds =  function () {
+        return data.map(function (task) {
+            let newTask = {...task};
             newTask.task_id = uuidv4();
-            console.log(newTask)
             return newTask;
         });
-
-        console.log(newTasks);
     }
-    
-    const addTask = function(){
-        const newTask = {task_name: newTaskName, task_id: new Date().getTime()}
+
+    const [tasks, setTasks] = useState(generateDataWithIds());
+    const [newTaskName, setNewTaskName] = useState("input default value");
+
+    const addTask = function() {
+        const newTask = { task_name: newTaskName, task_id: uuidv4() };
         const newTasks = [...tasks, newTask];
-        
         setTasks(newTasks);
+        setNewTaskName("");
     }
 
-    const completeTask = function (taskName) {
+    const completeTask = function (taskId) {
         const filteredTasks = tasks.filter(function (task) {
-            return task.task_name !== taskName;
+            return task.task_id !== taskId;
         });
-
         setTasks(filteredTasks);
-    }    
-    
+    }
+
     const renderTasks = function () {
-        return(
-            <ul>
-                {mapTaskNamesToListElements()}                
+        return (
+            <ul className={styles.taskList}>
+                {mapTaskNamesToListElements()}
             </ul>
         )
     }
 
-    const mapTaskNamesToListElements = function(){
-        return tasks.map(function (task){
-            return <li key={task.task_id}>
-                {task.task_name}
-                <button onClick={() => completeTask(task.task_name)}>complete task</button>
-            </li>
+    const mapTaskNamesToListElements = function() {
+        return tasks.map(function (task) {
+            return (
+                <li className={styles.taskItem} key={task.task_id}>
+                    {task.task_name}
+                    <button className={styles.taskButton} onClick={() => completeTask(task.task_id)}>complete task</button>
+                </li>
+            )
         })
     }
 
-    
     const handleInputChange = function (e) {
         const inputValue = e.target.value;
         setNewTaskName(inputValue);
-    }
+    };
 
-return(
+    return(
         <div>
             <h2>Todo</h2>
-            <input onChange={(event)=>handleInputChange(event)}/> 
-            <button onClick={()=>addTask()}>add task</button>
+            <input
+                className={styles.taskInput}
+                value={newTaskName}
+                onChange={(event) => handleInputChange(event)}
+            />
+            <br/>
+            <button onClick={() => addTask()}>add task</button>
             {renderTasks()}
         </div>
     );
 }
 
 export default Todo;
-
-    
